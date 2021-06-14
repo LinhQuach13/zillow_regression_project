d99166e3246f84ae3f9a86c5311ba5ee1d0d9d90
Linh Quach <Linh.M.Quach1@gmail.com>
2021-06-14 16:19:54 -0500

    Adding acquire file

---
+++ acquire.py
@@ -0,0 +1,65 @@
+import pandas as pd
+import numpy as np
+import os
+# acquire
+from env import host, user, password
+from pydataset import data
+
+# visualize
+import seaborn as sns
+import matplotlib.pyplot as plt
+plt.rc('figure', figsize=(11, 9))
+plt.rc('font', size=13)
+
+# turn off pink warning boxes
+import warnings
+warnings.filterwarnings("ignore")
+
+os.path.isfile('zillow_df.csv')
+
+
+# Create helper function to get the necessary connection url.
+def get_connection(db, user=user, host=host, password=password):
+    '''
+    This function uses my info from my env file to
+    create a connection url to access the Codeup db.
+    '''
+    return f'mysql+pymysql://{user}:{password}@{host}/{db}'
+
+
+
+# Use the above helper function and a sql query in a single function.
+def new_zillow_data():
+    '''
+    This function reads data from the Codeup db into a df.
+    '''
+    zillow_sql = ''' SELECT bedroomcnt, bathroomcnt, calculatedfinishedsquarefeet, taxvaluedollarcnt, yearbuilt, taxamount, fips, properties_2017.parcelid
+    FROM properties_2017 
+    JOIN predictions_2017 ON properties_2017.parcelid = predictions_2017.parcelid
+    JOIN propertylandusetype as plt ON properties_2017.propertylandusetypeid = plt.propertylandusetypeid
+    WHERE (predictions_2017.transactiondate BETWEEN '2017-05-01' AND '2017-08-31') and properties_2017.propertylandusetypeid IN (31, 46, 47, 260,
+    261,262,263,264,265,268,273,274,275, 276, 279)
+    '''
+    return pd.read_sql(zillow_sql, get_connection('zillow'))
+
+def get_zillow_data(cached=False):
+    '''
+    This function reads in telco churn data from Codeup database and writes data to
+    a csv file if cached == False or if cached == True reads in telco df from
+    a csv file, returns df.
+    '''
+    if cached == False or os.path.isfile('zillow_df.csv') == False:
+        
+        # Read fresh data from db into a DataFrame.
+        df = new_zillow_data()
+        
+        # Write DataFrame to a csv file.
+        df.to_csv('zillow_df.csv')
+        
+    else:
+        
+        # If csv file exists or cached == True, read in data from csv.
+        df = pd.read_csv('zillow_df.csv', index_col=0)
+        
+    return df
+
